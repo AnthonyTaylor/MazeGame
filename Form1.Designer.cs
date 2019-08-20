e05a907d479c28f7e0f6458806fce128799cbf3c
AnthonyTaylor <AnTaylor@Northamptonshire.gov.uk>
2019-08-20 12:01:37 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace MazeGame
+{
+    partial class MainScene
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlExit = new System.Windows.Forms.Panel();
+            this.pbPlayer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlExit
+            // 
+            this.pnlExit.BackColor = System.Drawing.Color.LightGreen;
+            this.pnlExit.Location = new System.Drawing.Point(724, -1);
+            this.pnlExit.Name = "pnlExit";
+            this.pnlExit.Size = new System.Drawing.Size(77, 451);
+            this.pnlExit.TabIndex = 1;
+            // 
+            // pbPlayer
+            // 
+            this.pbPlayer.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.pbPlayer.Location = new System.Drawing.Point(12, 12);
+            this.pbPlayer.Name = "pbPlayer";
+            this.pbPlayer.Size = new System.Drawing.Size(100, 50);
+            this.pbPlayer.TabIndex = 2;
+            this.pbPlayer.TabStop = false;
+            // 
+            // MainScene
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbPlayer);
+            this.Controls.Add(this.pnlExit);
+            this.Name = "MainScene";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MazeGame";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.NainScene_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnlExit;
+        private System.Windows.Forms.PictureBox pbPlayer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MazeGame
+{
+    public partial class MainScene : Form
+    {
+
+        public readonly int playerSpeed = 5;
+
+        /// <summary>
+        /// Prevent window resizing
+        /// </summary>
+        public MainScene()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        /// <summary>
+        /// Initial setup
+        /// </summary>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //window
+            DoubleBuffered = true;
+            BackColor = Color.Black;
+
+            //player
+            pbPlayer.Image = Properties.Resources.Player_sprite;
+            pbPlayer.Size = new Size(200,200);
+            pbPlayer.Location = new Point(0, 0);
+            pbPlayer.BorderStyle = BorderStyle.FixedSingle;
+            pbPlayer.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbPlayer.BackColor = TransparencyKey;
+
+            //exit zone
+            pnlExit.BackColor = Color.DarkGray;
+        }
+
+        /// <summary>
+        /// Player movement
+        /// </summary>
+        private void NainScene_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Player reached exit
+            if (pbPlayer.Bounds.IntersectsWith(pnlExit.Bounds))
+                Winner();
+
+            //Move player
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                case Keys.Up:
+                    pbPlayer.Top -= playerSpeed;
+                    break;
+                case Keys.A:
+                case Keys.Left:
+                    pbPlayer.Left -= playerSpeed;
+                    break;
+                case Keys.S:
+                case Keys.Down:
+                    pbPlayer.Top += playerSpeed;
+                    break;
+                case Keys.D:
+                case Keys.Right:
+                    pbPlayer.Left += playerSpeed;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        void Winner()
+        {
+            MessageBox.Show("Winner!");
+            Application.Exit();
+        }
+    }
+}
